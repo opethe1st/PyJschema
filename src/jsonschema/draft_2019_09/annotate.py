802d3d22e163449556c5ef5d1ca9dcebab46098a
Opemipo Ogunkola <ogunks@live.com>
2019-11-02 22:06:07 +0000

    add annotate.py and utils.py

---
+++ src/jsonschema/draft_2019_09/annotate.py
@@ -0,0 +1,30 @@
+import typing as t
+
+import dataclasses
+from .utils import append
+
+@dataclasses.dataclass
+class Instance:
+    value: t.Any
+    location: str
+
+
+def annotate(obj, location="") -> Instance:
+    if isinstance(obj, list):
+        return Instance(
+            value=[
+                annotate(obj=value, location=append(location, i))
+                for i, value in enumerate(obj)
+            ],
+            location=location,
+        )
+    elif isinstance(obj, dict):
+        return Instance(
+            value={
+                key: annotate(obj=value, location=append(location, key))
+                for key, value in obj.items()
+            },
+            location=location,
+        )
+    else:
+        return Instance(value=obj, location=location)
