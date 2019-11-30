7783f6ad8c831c3772ab90d3c59c589ba67fb534
Opemipo Ogunkola <ogunks@live.com>
2019-11-30 10:50:27 +0000

    add missing file

---
+++ src/jschema/draft_2019_09/types/type_base.py
@@ -0,0 +1,57 @@
+import itertools
+import typing as t
+
+from jschema.common import AValidator, KeywordGroup, ValidationError
+
+from .common import validate_instance_against_all_validators
+
+
+class Type(AValidator):
+    """Validator for a type"""
+
+    KEYWORDS_TO_VALIDATOR: t.Dict[
+        t.Tuple[str, ...], t.Union[t.Type[KeywordGroup], t.Type[KeywordGroup]]
+    ] = {}
+    type_: t.Optional[t.Type] = None
+
+    def __init__(self, schema):
+        self._validators: t.List[AValidator] = []
+        for keywords in self.KEYWORDS_TO_VALIDATOR:
+
+            if any(schema.get(keyword) is not None for keyword in keywords):
+                self._validators.append(
+                    self.KEYWORDS_TO_VALIDATOR[keywords](
+                        **{keyword: schema.get(keyword) for keyword in keywords}
+                    )
+                )
+
+    def validate(self, instance):
+        messages = []
+        if self.type_ is not None and not isinstance(instance, self.type_):
+            messages.append(f"instance is not a {self.type_}")
+        if messages:
+            return ValidationError(messages=messages)
+
+        error_generator = validate_instance_against_all_validators(
+            validators=self._validators, instance=instance
+        )
+        # default to True if exhausted since that means that there were no errors
+        first_error = next(error_generator, True)
+
+        # needs to be not - because bool(first_error) evaluates to True
+        if not first_error:
+            return ValidationError(
+                messages=messages,
+                children=itertools.chain(
+                    [first_error], error_generator
+                ),
+            )
+        elif messages:
+            return ValidationError(
+                messages=messages
+            )
+        else:
+            return True
+
+    def subschema_validators(self):
+        yield from self._validators
