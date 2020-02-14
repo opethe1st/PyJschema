704e37cc58c65afd44c55271f8ab60f4d20a1b57
Opemipo Ogunkola <ogunks900@gmail.co>
2020-02-14 22:19:32 +0000

    WIP

--- src/pyjschema/draft_2019_09/validator_test.py
+++ src/pyjschema/draft_2019_09/validator_test.py
@@ -41,3 +41,41 @@ class TestValidator(unittest.TestCase):
             result,
             bool(validator.validate(instance=instance)),
         )
+
+
+# hm, recursiveRef only works if there is the ability to combine with ref with adjacent keywords.
+# is this too difficult?
+class TestRecursiveRef(unittest.TestCase):
+    @parameterized.parameterized.expand(
+        [
+            ({"data": [{"data": "innner data"}], "children": []}, True),
+
+        ]
+    )
+    def test_recursive_ref(self, instance, result):
+        schema = {
+            "$schema": "schema",
+            "$id": "https://example.com/schema",
+            "$ref": "tree",
+            "$defs": {
+                "tree": {
+                    "$id": "tree",
+                    "type": "object",
+                    "properties": {
+                        "data": True,
+                        "children": {
+                            "type": "array",
+                            "items": {"$ref": "tree"},
+                        }
+                    },
+                    "required": ["data", "children"]
+                }
+            }
+        }
+
+        validator, uri_to_validator = build_validator_and_resolve_references(schema=schema)
+
+        self.assertEqual(
+            result,
+            bool(validator.validate(instance=instance)),
+        )
