a06d2eabfb3fb627521778ede165b0d9a6281292
Opemipo Ogunkola <ogunks@live.com>
2020-02-28 18:56:12 +0000

    use urljoin

--- src/pyjschema/utils.py
+++ src/pyjschema/utils.py
@@ -1,25 +1,10 @@
 import contextlib
 import contextvars
-
-from uritools import urisplit, uriunsplit
+from urllib import parse
 
 
 def to_canonical_uri(current_base_uri, uri):
-    split = urisplit(uri)
-    if split.scheme is None and split.authority is None:
-        current_base_uri_split = urisplit(current_base_uri)
-        path = split.path if split.path and split.path[0] == "/" else "/" + split.path
-        path = current_base_uri_split.path if path in ["/", ""] else path
-        return uriunsplit(
-            [
-                current_base_uri_split.scheme,
-                current_base_uri_split.authority,
-                path,
-                split.query,
-                split.fragment,
-            ]
-        )
-    return uri
+    return parse.urljoin(current_base_uri, uri)
 
 
 @contextlib.contextmanager
